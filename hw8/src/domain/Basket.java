2d5017e17e0fd8ee73866ab47b18d86ad202b181
mohammad nasr <nasrmohammad4804@gmail.com>
2021-07-26 07:05:25 +0430

    create class Basket witch 2 property 1 object of Customer and 1 object of Product and add Object CustomerRepo and OrderDetailsRepo and OrderRepo and ProductRepo and BasketRepo that static in ApplicationContext class

---
+++ hw8/src/domain/Basket.java
@@ -0,0 +1,35 @@
+package domain;
+
+public class Basket {
+    private Customer customer;
+    private Product product;
+
+    public Basket(Customer customer, Product product) {
+        this.customer = customer;
+        this.product = product;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "Basket{" +
+                "customer=" + customer +
+                ", product=" + product +
+                '}';
+    }
+}
--- hw8/src/service/ApplicationContext.java
+++ hw8/src/service/ApplicationContext.java
@@ -1,14 +1,41 @@
 package service;
 
+import repository.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ApplicationContext {
     private static Connection connection;
+    private static CustomerRepo customerRepo;
+    private static OrderDetailsRepo orderDetailsRepo;
+    private static OrderRepo orderRepo;
+    private static ProductRepo productRepo;
+    private static BasketRepo basketRepo;
 
     public static Connection getConnection() {
         return connection;
+    }
+
+    public static CustomerRepo getCustomerRepo() {
+        return customerRepo;
+    }
+
+    public static OrderDetailsRepo getOrderDetailsRepo() {
+        return orderDetailsRepo;
+    }
+
+    public static OrderRepo getOrderRepo() {
+        return orderRepo;
+    }
+
+    public static ProductRepo getProductRepo() {
+        return productRepo;
+    }
+
+    public static BasketRepo getBasketRepo() {
+        return basketRepo;
     }
 
     public ApplicationContext(){
@@ -18,5 +45,11 @@ public class ApplicationContext {
         }catch (SQLException e){
             System.out.println(e.getErrorCode());
         }
+
+        customerRepo=new CustomerRepo();
+        orderDetailsRepo=new OrderDetailsRepo();
+        orderRepo=new OrderRepo();
+        productRepo=new ProductRepo();
+        basketRepo=new BasketRepo();
     }
 }
